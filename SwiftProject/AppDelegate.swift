23a7f4c813891f032562ee532634f65fe5b645d5
wujunyang <wujunyang@126.com>
2017-06-29 20:45:11 +0800

    no message

--- SwiftProject/AppDelegate.swift
+++ SwiftProject/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
+    //patch测试
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         //调用SwiftAttributeTest的方法
